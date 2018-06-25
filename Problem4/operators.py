3111ce538938f1fccc9ee8192f40fae75a634d89
krista <kristapeterson144@gmail.com>
2018-06-25 14:54:18 -0700

    Finished through problem 5

--- Problem4/operators.py
+++ Problem4/operators.py
@@ -1,4 +1,6 @@
 """Assignment 1: Problem 4: Operators"""
+import math
+
 '''
 PART 1
 
@@ -7,20 +9,24 @@ Understanding opearators
 '''
 
 # TODO 1. Print the result of 5 plus 2
+print (5+2)
 
 # TODO 2. Print the result of 5 minus 2
+print (5-2)
 
 # TODO 3. Print the result of 5 times 2
+print (5*2)
 
 # TODO 4. Print the result of 5 divided by 2
-
+print (5/2)
 # TODO 5. Print the result of floor division or truncation division 5 // 2
-
+print (5//2)
 # TODO 6. Print the remainder of 5 / 2 using the modulo operator (%)
-
+print (5%2)
 # TODO 7. Print the result of 5 raised to the power of 2
-
+print (5**2)
 # TODO 8. Print the square root of 25
+print (math.sqrt(25))
 
 '''
 PART 2
@@ -30,14 +36,15 @@ each as a variable and print the variable.
 '''
 
 # TODO 9. Equation 1
-
+print ((3*5)/(2+3))
 # TODO 10. Equation 2
-
+print ((math.sqrt(7+9))*2)
 # TODO 11. Equation 3
-
+print ((4-7)**3)
 # TODO 12. Equation 4
-
+print ((math.sqrt(-19+100))**(1/4))
 # TODO 13. Equation 5
+print (6%4)
 
 '''
 PART 3
@@ -45,3 +52,6 @@ PART 3
 Write two equations with the same operands and operators but that evaluate to different values. Store each in a
 separate variable, then print the variables.
 '''
+print (2+3*5)
+print ((2+3)*5)
+
--- Problem5/user_input.py
+++ Problem5/user_input.py
@@ -28,15 +28,20 @@ October 20 1977
 '''
 
 # TODO 1. Ask for the first name and store it in a variable
+first= input ("what is your first name")
 
 # TODO 2. Ask for the last name and store it in a variable
+last= input ("what is your last name")
 
 # TODO 3. Print "Enter your date of birth:"
+print ("enter your date of birth")
 
 # TODO 4. Ask for the Month and store it in a variable
-
+mo= input ("what is the month")
 # TODO 5. Ask for the day and store it in a variable
+day= input ("what is the day")
 
 # TODO 6. Ask for the year and store it in a variable
-
+year= input ("what is the year")
 # TODO 7. Print the name with the date of birth as shown above.
+print (first, last, "was born on", mo, day, year)
